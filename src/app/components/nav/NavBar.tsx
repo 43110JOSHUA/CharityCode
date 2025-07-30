0ca09b9e9a8c7a8199249bab9b4107e06574caf9
43110JOSHUA <joshuaatang@gmail.com>
2025-07-31 02:21:40 +1000

    Fixed name from wrapping

--- src/app/components/nav/NavBar.tsx
+++ src/app/components/nav/NavBar.tsx
@@ -10,7 +10,9 @@ const NavBar = () => {
     <nav className="bg-body-tertiary border-bottom d-flex gap-3 justify-content-between align-items-center py-2 px-3 px-md-5  ">
       <div className="d-flex gap-2 align-items-center">
         <Avatar src="/CCicon.png" />
-        <h4 className="pt-2 fw-bold d-none d-sm-block">Charity Code</h4>
+        <h4 className="pt-2 fw-bold d-none d-sm-block text-nowrap">
+          Charity Code
+        </h4>
       </div>
       <SearchBar />
       <IconButton
