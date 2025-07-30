0ade78d01e6b2cec1652c8f33878b4e9bff1a37e
43110JOSHUA <joshuaatang@gmail.com>
2025-07-31 01:22:47 +1000

    Added IconButton component

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -5,9 +5,9 @@ import Avatar from "./Avatar";
 const NavBar = () => {
   return (
     <nav className="bg-body-tertiary d-flex justify-content-between align-items-center py-2 px-4 px-md-5  ">
-      <div className="d-flex d-wrap gap-3 align-items-center">
+      <div className="d-flex d-wrap gap-2 align-items-center">
         <Avatar src="/CCicon.png" />
-        <h4 className="pt-2">Charity Code</h4>
+        <h4 className="pt-2 fw-bold">Charity Code</h4>
       </div>
     </nav>
   );
