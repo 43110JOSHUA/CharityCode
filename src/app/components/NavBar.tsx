2919e9e228dcb7631b6adfd15e92cc7887018c8d
43110JOSHUA <joshuaatang@gmail.com>
2025-07-31 01:13:26 +1000

    Added Charity Code icon to header

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "next/image";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
   return (
-    <nav className="d-flex justify-content-between align-items-center py-2 px-2 px-md-3">
-      <h1>Charity Code</h1>
+    <nav className="bg-body-tertiary d-flex justify-content-between align-items-center py-2 px-2 px-md-5  ">
+      <Avatar src="/CCicon.png" />
       <h2>Your tagline here</h2>
-      <button className="btn btn-primary">Donate</button>
     </nav>
   );
 };
