519d739cc5bafeee488d2051348edd23b20c3ffd
43110JOSHUA <joshuaatang@gmail.com>
2025-08-14 19:41:54 +1000

    Fixed error message about smooth scrolling

--- app/components/nav/SideNav.tsx
+++ app/components/nav/SideNav.tsx
@@ -38,7 +38,7 @@ const SideNav: React.FC<SideNavProps> = ({ isOpen, onClose }) => {
 
         {/* Content */}
         <div className="p-3">
-          <div className="d-grid gap-3">
+          <div className="d-grid gap-2">
             <Link href="/explore" className="text-decoration-none">
               <button className="btn theme-btn btn-tan border-border-tan text-muted w-100">
                 Explore
