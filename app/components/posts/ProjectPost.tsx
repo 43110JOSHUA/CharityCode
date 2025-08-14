d05f767ad31169a2df9c308bd87464dd03db3cf8
43110JOSHUA <joshuaatang@gmail.com>
2025-08-14 16:34:59 +1000

    Fixed Post badge stretching

--- app/components/posts/ProjectPost.tsx
+++ app/components/posts/ProjectPost.tsx
@@ -21,14 +21,14 @@ const ProjectPost: React.FC<ProjectPostProps> = ({
     <div className="card my-2 bg-light-tan">
       <div className="card-body">
         <div className="d-flex justify-content-between align-items-top gap-2 mb-2">
-          <div className="d-flex gap-2 flex-wrap">
+          <div className="d-flex flex-wrap gap-2">
             <h5 className="card-title mb-0">{title}</h5>
             <span className="text-muted small mt-1">
               {username} • {datePosted}
             </span>
           </div>
           <span
-            className={`badge px-3 py-2 fs-6 ${
+            className={`badge px-3 py-2 fs-6 mb-auto ${
               status === "Open" ? "bg-success" : "bg-secondary"
             }`}
           >
