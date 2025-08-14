aba637c7c73b1cc1825f6b7c442e71a746cbfe2d
43110JOSHUA <joshuaatang@gmail.com>
2025-08-14 20:27:47 +1000

    Updated logo button

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -5,6 +5,7 @@ import MenuButton from "./MenuButton";
 import SearchBar from "./SearchBar";
 import SideNav from "./SideNav";
 import PageBorder from "../PageBorder";
+import Link from "next/link";
 
 const NavBar = () => {
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
@@ -19,10 +20,14 @@ const NavBar = () => {
         <PageBorder>
           <nav className="d-flex gap-3 justify-content-between align-items-center py-2 px-3">
             <div className="d-flex gap-2 align-items-center">
-              <Avatar src="/CCicon.png" />
-              <h4 className="pt-2 fw-bold d-none d-sm-block text-nowrap mb-2">
-                Charity Code
-              </h4>
+              <Link href="/" className="text-decoration-none">
+                <Avatar src="/CCicon.png" />
+              </Link>
+              <Link href="/" className="text-decoration-none text-dark">
+                <h4 className="pt-2 fw-bold d-none d-sm-block text-nowrap mb-2">
+                  Charity Code
+                </h4>
+              </Link>
             </div>
             <SearchBar />
             <MenuButton icon="bi bi-list" onClick={toggleSideNav} />
