e80f5ccd85f2d1d2361c6e8325b94aca98a502e9
43110JOSHUA <joshuaatang@gmail.com>
2025-10-06 23:53:44 -0700

    Made header for postfeed dynamic

--- app/components/posts/PostFeed.tsx
+++ app/components/posts/PostFeed.tsx
@@ -16,6 +16,7 @@ export default function PostFeed() {
   const [posts, setPosts] = useState<
     QueryDocumentSnapshot<DocumentData, DocumentData>[]
   >([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const q = query(
@@ -24,19 +25,30 @@ export default function PostFeed() {
     );
 
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      const snapshotDocs = snapshot.docs
-
-      setPosts(snapshotDocs)
+      const snapshotDocs = snapshot.docs;
+      setPosts(snapshotDocs);
+      setLoading(false);
     });
 
     return unsubscribe;
   }, []);
 
   return (
-    <div className="container py-3 d-flex flex-column align-items-center justify-content-center">
-      {posts.map((post) => (
-        <ProjectPost key={post.id} data={post.data()} id={post.id} />
-      ))}
+    <div>
+      <div className="text-center border-bottom pt-5 py-2">
+        <h3>
+          {loading
+            ? "Loading Projects..."
+            : `Explore ${posts.length} Project ${
+                posts.length === 1 ? "Idea" : "Ideas"
+              }`}
+        </h3>
+      </div>
+      <div className="container py-3 d-flex flex-column align-items-center justify-content-center">
+        {posts.map((post) => (
+          <ProjectPost key={post.id} data={post.data()} id={post.id} />
+        ))}
+      </div>
     </div>
   );
 }
--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -5,9 +5,6 @@ export default function Explore() {
   return (
     <div className="bg-light-tan min-vh-100">
       <PageBorder>
-        <div className="text-center border-bottom pt-5 py-2">
-          <h3>Explore 2 Project Ideas</h3>
-        </div>
         <PostFeed />
       </PageBorder>
     </div>
