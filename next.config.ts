4d13f047d36e9bc5c861ba6323719c1159f0f0a6
43110JOSHUA <joshuaatang@gmail.com>
2025-10-09 11:26:41 -0700

    Test deploy with site stopped

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Optimize for Azure deployment
+  reactStrictMode: true,
+  distDir: 'build',
   output: 'standalone',
   // Reduce memory usage during build
   experimental: {
