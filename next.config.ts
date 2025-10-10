4aa36465cacb5cd7597c727393d197cad5b87411
43110JOSHUA <joshuaatang@gmail.com>
2025-10-09 17:05:33 -0700

    Suppress scss warnings

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   experimental: {
     workerThreads: false,
   },
+
+  // Suppress SASS deprecation warnings from Bootstrap
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api', 'import'],
+    quietDeps: true,
+  },
 };
 
 export default nextConfig;
