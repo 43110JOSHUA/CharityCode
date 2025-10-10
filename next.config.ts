a6db4c57fe7ccde3854d506a13df7e3a59750077
43110JOSHUA <joshuaatang@gmail.com>
2025-10-09 17:27:03 -0700

    Updated terms page

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,6 @@ const nextConfig: NextConfig = {
   experimental: {
     workerThreads: false,
   },
-
-  // Suppress SASS deprecation warnings from Bootstrap
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api', 'import'],
-    quietDeps: true,
-  },
 };
 
 export default nextConfig;
